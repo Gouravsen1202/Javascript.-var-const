2bfd51e92695f741b511f9c833901935cf8ae605
Gouravsen1202 <gouravsen757@gmail.com>
2024-12-25 20:44:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// function banana hai
+function sid(){
+
+    // var a=5;
+    // var b=10;
+    // var c=a+b;
+    // console.log(c);
+
+}
+
+// ye function call hua hai
+sid();
+
+// 1 hi variable 2 bar le lete hai var me to wo uski value update kr deta hai
+
+
+
+// lekin ye let aur const keyword me nhi kar sakte agar karege to wo arror show karega isme allow nhi hia 
+
+
+
+
+// var a=5;
+// a=10;
+// console.log(a)
+// // reassiment var aur const me same variable allow hia krna
+
+
+
+// let a=5;
+// {
+//     let a=9;
+//     console.log(a);
+// }
+// console.log(a);
+
+// isme jo value bracit me {}rheti hai wo print hogi aur isme update nhi hota hia 
+
+
+
+
+
+// hosting sirf var me saport karta hai bus
